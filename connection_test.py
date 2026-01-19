25c41508cbcd64e52c5aa47c76c2c37e05481d7d
Samartha Bashyal <samarthabashyal0373@gmail.com>
2026-01-19 22:31:44 +0000

    Delete connection_test.py

--- connection_test.py
+++
@@ -1,9 +0,0 @@
-import get_connection
-
-def test_conn():
-    """checks whether get_connection returns connection to mysql."""
-    con = get_connection.get_con()
-    if con:
-        print('successful!!!')
-    else:
-        print('unsuccessful')
