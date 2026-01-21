98e0fa0325d0068ba0fa1d5866c4efd3dd0d84fd
Samartha Bashyal <samarthabashyal0373@gmail.com>
2026-01-21 14:52:49 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import create_database as cd
 import create_tables as ct
 import insert_data as id
+print('creating database')
 cd.create_database()
+print('creating tables')
 ct.create_tables()
+print('adding customers')
 id.add_customer()
+print('creating customer account')
 id.customer_signing()
